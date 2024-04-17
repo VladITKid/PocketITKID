657bc4612cd0b18f967532ef11a94f1eabd64085
VladITKid <vladitkid@gmail.com>
2024-04-17 12:27:42 +0300

    удалил vars.js и папку ../html/common

--- dist/js/main.js
+++ dist/js/main.js
@@ -18,6 +18,12 @@
 — — — — — — — — — — — — — 
 */
 // todo: Смена страниц по клику
+// Логотип
+const headerLogo = document.querySelector('.header__logo');
+// Вызываю кнопки из меню шапки и преобразую в массив
+const headerMenuBtnsArray = Array.from(document.querySelectorAll('.header__menu-btn'));
+// Вызываю кнопки из нижней навигации и преобразую в массив
+const bottomNavBtnsArray = Array.from(document.querySelectorAll('.bottom-nav__btn'));
 (() => {
     // * Функция смены страниц по клику
 
